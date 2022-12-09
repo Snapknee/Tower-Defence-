0be14b8bd8f3afdf036b759a2d71607bfea0d97a
Snapknee <joshuadanielprice98@gmail.com>
2022-12-09 20:29:52 +0000

    Count down wave timer & Textmesh
    This contains code and assets for creating a count down timer that will count down till the next wave spawns. This also contains textmesh which includes different text types

---
+++ Tower Defence/Assets/Scripts/Enemy/WaveSpawner.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+
+    public Text waveCountdownText;
+
+    private int WaveIndex = 0;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        
+        //Reduce countdown time by 1 every second
+        countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave ()
+    {
+        Debug.Log("Next Wave Incoming");
+        WaveIndex++;
+        for (int i = 0; i < WaveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
